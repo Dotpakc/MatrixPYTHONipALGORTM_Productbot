36a9222924293e838aeb9a165b8d1e70772717e1
DotPack <lifirenko123@gmail.com>
2023-06-15 20:08:30 +0300

    add shop

--- handlers/shop.py
+++ handlers/shop.py
@@ -1,3 +1,8 @@
 from aiogram import types
 
 from loader import dp
+from keyboards.main_kb import main_back_keyboard
+
+@dp.callback_query_handler(text='courses')
+async def process_callback_kb1btn1(callback_query: types.CallbackQuery):
+    await callback_query.message.edit_text("В процесі розробки", reply_markup=main_back_keyboard)
